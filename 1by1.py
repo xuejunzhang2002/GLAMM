e9c2c64f2fb9a6813ce5aa36a3a4ddd8a4d2557c
Xuejun Zhang <xuejunzh@sled-aspen.eecs.umich.edu>
2024-06-08 20:25:50 -0400

    prompt updated

--- 1by1.py
+++ 1by1.py
@@ -317,20 +317,20 @@ if __name__ == "__main__":
     for item in data[:10]:
         if args.dataset_type=='validation':
             if item['data_source']=='COCO':
-                input_str = """Select the class for <bbox> from: dining table, cow, spoon, potted plant, zebra, donut, traffic light, backpack, vase, sports ball, person, apple, chair, bicycle, pizza, remote, tennis racket, bench, boat, motorcycle, bottle, broccoli, suitcase, dog, orange, skis, cake, umbrella, cell phone, bowl, wine glass, surfboard, handbag, sheep, tie, banana, sink, kite, clock, cup, giraffe, bus, knife, bird, elephant, truck, book, car, tv, carrot."""
+                input_str = """what is the class of <bbox>"""
                 categories=[
         'dining table', 'cow', 'spoon', 'potted plant', 'zebra', 'donut', 'traffic light', 'backpack', 'vase', 'sports ball', 'person', 'apple', 'chair', 'bicycle', 'pizza', 'remote', 'tennis racket', 'bench', 'boat', 'motorcycle', 'bottle', 'broccoli', 'suitcase', 'dog', 'orange', 'skis', 'cake', 'umbrella', 'cell phone', 'bowl', 'wine glass', 'surfboard', 'handbag', 'sheep', 'tie', 'banana', 'sink', 'kite', 'clock', 'cup', 'giraffe', 'bus', 'knife', 'bird', 'elephant', 'truck', 'book', 'car', 'tv', 'carrot']
             elif item['data_source']=='ADE':
-                input_str = """Select the class for <bbox> from: plant, window, glass, windshield, vase, mirror, tree, ceiling, cabinet, rock, person, bag, chair, door, light, food, arm, base, bottle, brand, grass, box, pole, license plate, curtain, plate, mountain, table, head, building, balcony, shelf, pillow, column, shutter, flowerpot, leg, apron, sign, picture, cushion, flower, drawer, wheel, roof, book, price tag, car, rim, handle."""
+                input_str = """what is the class of <bbox>"""
                 categories=[
         'plant', 'window', 'glass', 'windshield', 'vase', 'mirror', 'tree', 'ceiling', 'cabinet', 'rock', 'person', 'bag', 'chair', 'door', 'light', 'food', 'arm', 'base', 'bottle', 'brand', 'grass', 'box', 'pole', 'license plate', 'curtain', 'plate', 'mountain', 'table', 'head', 'building', 'balcony', 'shelf', 'pillow', 'column', 'shutter', 'flowerpot', 'leg', 'apron', 'sign', 'picture', 'cushion', 'flower', 'drawer', 'wheel', 'roof', 'book', 'price tag', 'car', 'rim', 'handle']
         elif args.dataset_type=='test':
             if item['data_source']=='COCO':
-                input_str = """Select the class for <bbox> from: apple, backpack, banana, bench, bicycle, bird, boat, book, bottle, bowl, broccoli, bus, cake, car, carrot, cell phone, chair, clock, couch, cow, cup, dining table, dog, donut, elephant, handbag, horse, kite, knife, motorcycle, orange, person, pizza, potted plant, remote, sheep, sink, skateboard, skis, spoon, sports ball, suitcase, surfboard, tie, traffic light, truck, tv, umbrella, vase, wine glass."""
+                input_str = """what is the class of <bbox>"""
                 categories=[
         'apple', 'backpack', 'banana', 'bench', 'bicycle', 'bird', 'boat', 'book', 'bottle', 'bowl', 'broccoli', 'bus', 'cake', 'car', 'carrot', 'cell phone', 'chair', 'clock', 'couch', 'cow', 'cup', 'dining table', 'dog', 'donut', 'elephant', 'handbag', 'horse', 'kite', 'knife', 'motorcycle', 'orange', 'person', 'pizza', 'potted plant', 'remote', 'sheep', 'sink', 'skateboard', 'skis', 'spoon', 'sports ball', 'suitcase', 'surfboard', 'tie', 'traffic light', 'truck', 'tv', 'umbrella', 'vase', 'wine glass']
             elif item['data_source']=='ADE':
-                input_str = """Select the class for <bbox> from: arm, armchair, bed, book, bottle, box, building, cabinet, car, ceiling, chair, column, curtain, cushion, door, drawer, fence, floors, flower, glass, grass, handle, head, lamp, leg, light, light source, mirror, mountain, pane, person, picture, pillow, plant, plate, pole, pot, road, rock, seat, shelf, sign, sofa, spotlight, streetlight, table, tree, vase, wheel, window."""
+                input_str = """what is the class of <bbox>"""
                 categories=['arm', 'armchair', 'bed', 'book', 'bottle', 'box', 'building', 'cabinet', 'car', 'ceiling', 'chair', 'column', 'curtain', 'cushion', 'door', 'drawer', 'fence', 'floors', 'flower', 'glass', 'grass', 'handle', 'head', 'lamp', 'leg', 'light', 'light source', 'mirror', 'mountain', 'pane', 'person', 'picture', 'pillow', 'plant', 'plate', 'pole', 'pot', 'road', 'rock', 'seat', 'shelf', 'sign', 'sofa', 'spotlight', 'streetlight', 'table', 'tree', 'vase', 'wheel', 'window']
         folder_path = item['folder']
         folder_path = folder_path.replace('bbox', 'raw') 
